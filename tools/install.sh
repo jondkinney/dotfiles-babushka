5834ca2329bb3eddb28908956c9bd57ea6fd47de
Jon Kinney <jonkinney@gmail.com>
2011-09-22 04:57:07 -0500

    update

--- tools/install.sh
+++ tools/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd &&
-[ -d '~/.j2lfy-dotfiles' ] || git clone git://github.com/j2fly/dotfiles.git ~/.j2lfy-dotfiles &&
-find ~/.j2fly-dotfiles/home -depth 1 | while read f; do
+[ -d '.j2lfy-dotfiles' ] || git clone git://github.com/j2fly/dotfiles.git .j2lfy-dotfiles &&
+find .j2fly-dotfiles/home -depth 1 | while read f; do
   #[ "$f" == '.j2lfy-dotfiles/files/.git' ] ||
   ([ "$f" == '.j2lfy-dotfiles/home/.oh-my-zsh' ] && ln -vsf "$f" .oh-my-zsh) ||
+  ([ "$f" == '.j2lfy-dotfiles/home/.ssh' ] && sudo ln -vsf "$f" .ssh) ||
   ln -vsf "$f" .
 done
 
