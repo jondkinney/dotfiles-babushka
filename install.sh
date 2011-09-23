66c6200f6bf2dd761316d392b3c6ad8a699039a4
Jon Kinney <jonkinney@gmail.com>
2011-09-22 21:16:37 -0500

    put the install at the root

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cd &&
-[ -d "/Users/`whoami`/.j2fly-dotfiles" ] || git clone git://github.com/j2fly/dotfiles.git /Users/`whoami`/.j2fly-dotfiles &&
-find /Users/`whoami`/.j2fly-dotfiles/home -depth 1 | while read f; do
-  ln -vsf "$f" /Users/`whoami`/
+[ -d '.dotfiles' ] || git clone git://github.com/j2fly/dotfiles.git .dotfiles &&
+find .dotfiles/files -depth 1 | while read f; do
+  ([ "$f" == '.dotfiles/files/oh-my-zsh' ] && ln -vsf "$f" .oh-my-zsh) ||
+  ln -vsf "$f" .
 done
 
 if [ -d "/Users/`whoami`/.ssh" ]; then
