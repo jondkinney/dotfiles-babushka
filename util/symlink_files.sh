32baaed114a734f92feebdd57893ab8a4fe2ec5d
Jon Kinney <jonkinney@gmail.com>
2016-11-30 06:53:38 -0600

    Capital F will force a directory overwrite for ln -s

--- util/symlink_files.sh
+++ util/symlink_files.sh
@@ -11,13 +11,7 @@ function symlink_files(){
   for file_path in $files
   do
     file_name=$(basename $file_path)
-    if [[ -f "$destination_dir/$file_name" ]]; then
-      rm $destination_dir/$file_name
-    fi
-    if [[ -d "$destination_dir/$file_name" ]]; then
-      rm -r $destination_dir/$file_name
-    fi
-    ln -vnsf $file_path "$destination_dir/$file_name"
+    ln -vnsF $file_path "$destination_dir/$file_name"
   done
 
   shopt -u dotglob # Disable globbing over hidden files
