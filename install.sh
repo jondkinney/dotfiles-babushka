90cc9e97a56c3c3e672bb087278d92b61c48c6f4
Jon Kinney <jonkinney@gmail.com>
2018-10-18 15:35:22 -0500

    remove questions

--- install.sh
+++ install.sh
@@ -14,15 +14,15 @@ ln -vnsf $HOME/.dotfiles/zsh_conf/j2fly.zsh-theme $HOME/.oh-my-zsh/themes
 ln -vnsf $HOME/.dotfiles/scripts/pair /usr/local/bin
 ln -vnsf $HOME/.dotfiles/files/karabiner.json $HOME/.config/karabiner
 
-read -p  "????? What name do you want to display on your git commits? "
-if [[ $REPLY != '' ]]; then
-  sed -i -e "s/Jon\ Kinney/$REPLY/" $HOME/.gitconfig_private
-fi
+# read -p  "????? What name do you want to display on your git commits? "
+# if [[ $REPLY != '' ]]; then
+#   sed -i -e "s/Jon\ Kinney/$REPLY/" $HOME/.gitconfig_private
+# fi
 
-read -p  "????? What email do you want to display on your git commits? "
-if [[ $REPLY != '' ]]; then
-  sed -i -e "s/jonkinney@gmail.com/$REPLY/" $HOME/.gitconfig_private
-fi
+# read -p  "????? What email do you want to display on your git commits? "
+# if [[ $REPLY != '' ]]; then
+#   sed -i -e "s/jonkinney@gmail.com/$REPLY/" $HOME/.gitconfig_private
+# fi
 
 echo 'Successfully Installed'
 echo '    ____        __  _____ __'
